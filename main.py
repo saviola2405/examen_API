4329c5ad5dd4dcd991853ee2801670acc153c9a3
Saviola_24 <saviolafenitr@gmail.com>
2025-07-24 09:43:40 +0300

    question 5

--- main.py
+++ main.py
@@ -38,3 +38,19 @@ async def add_students(new_students: list[Student]):
 @app.get("./student", response_model=list[Student], status_code=200)
 def get_students():
     return JSONResponse(content=students, status_code=200)
+
+@app.put("/students", response_model=list[Student], status_code=200)
+async def update_or_add_student(student: Student):
+    for i, existing_student in enumerate(students):
+        if existing_student.Reference == student.Reference:
+
+            if (existing_student.FirstName != student.FirstName or
+                    existing_student.LastName != student.LastName or
+                    existing_student.Age != student.Age):
+                students[i] = student
+
+            return JSONResponse(content=students, status_code=200)
+
+
+    students.append(student)
+    return JSONResponse(content=students, status_code=200)
